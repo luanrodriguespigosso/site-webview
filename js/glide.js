628049aa543d843ff3ccd6ec43f9713288f4b8a4
patrickhpgc <webviewtech@gmail.com>
2021-08-05 22:16:32 -0300

    upload inicial

---
+++ js/glide.js
@@ -0,0 +1,6 @@
+  const config = {
+    type: 'carousel',
+    perView: 3,
+    autoplay: 2500
+  }
+  new Glide('.glide', config).mount()
---
+++ js/index.js
@@ -0,0 +1,6 @@
+  const config = {
+    type: 'carousel',
+    perView: 1,
+    autoplay: 2500
+  }
+  new Glide('.glide', config).mount()
