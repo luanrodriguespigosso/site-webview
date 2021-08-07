e4a9dea238ea824c64faea0d7cad107fca5de12a
patrickhpgc <webviewtech@gmail.com>
2021-08-07 20:21:42 -0300

    transição do menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,30 @@
-  const config = {
-    type: 'carousel',
-    perView: 1,
-    autoplay: 2500
-  }
-  new Glide('.glide', config).mount()
+window.addEventListener("scroll", function(event) {
+
+    var scroll = this.scrollY;
+
+    console.log(scroll);
+    if (scroll >= 700) {
+        document.getElementById("menu").style.background = "rgba(255, 255, 255, 0.95)";
+        document.getElementById("menu").style.transition = "0.7s";
+        document.getElementById("menu").style.borderBottom = "1px solid rgba(32, 19, 77, 0.2)";
+        document.getElementById("logo-2").style.opacity = "0";
+
+        /* Muda cor dos links do menu */
+        document.getElementById("menuitem-1").style.color = "#20134D";
+        document.getElementById("menuitem-2").style.color = "#20134D";
+        document.getElementById("menuitem-3").style.color = "#20134D";
+        document.getElementById("menuitem-4").style.color = "#20134D";
+        document.getElementById("menuitem-5").style.color = "#20134D";
+    } else if (scroll < 700) {
+        document.getElementById("menu").style.background = "rgba(32, 19, 77, 0.0)";
+        document.getElementById("menu").style.transition = "0.7s";
+        document.getElementById("logo-2").style.opacity = "1";
+
+        /* Muda cor dos links do menu */
+        document.getElementById("menuitem-1").style.color = "#FFFFFF";
+        document.getElementById("menuitem-2").style.color = "#FFFFFF";
+        document.getElementById("menuitem-3").style.color = "#FFFFFF";
+        document.getElementById("menuitem-4").style.color = "#FFFFFF";
+        document.getElementById("menuitem-5").style.color = "#FFFFFF";
+    }
+});
