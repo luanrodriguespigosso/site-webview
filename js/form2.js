92b3e968dcf99177515b08e417146ae5b762aba9
patrickhpgc <webviewtech@gmail.com>
2021-09-10 22:00:06 -0300

    alterando mensagem de validação botão checkbox política

--- js/form2.js
+++ js/form2.js
@@ -10,3 +10,16 @@ document.querySelector('#name').oninvalid = function() {
         this.setCustomValidity("Caracter invalido! Insira somente letras.");
     }
 };
+
+document.querySelector('#check-politica').oninvalid = function() {
+
+    // remove mensagens de erro antigas
+    this.setCustomValidity("");
+
+    // reexecuta validação
+    if (!this.validity.valid) {
+
+        // se inválido, coloca mensagem de erro
+        this.setCustomValidity("Para prosseguir você precisa ler e aceitar a nossa política de privacidade.");
+    }
+};
